f5d233315d59a649ddcb1507b61e080eba816bc3
nishina <n.toshi.1.014@gmail.com>
2016-11-05 19:38:23 +0900

    Add store, Update package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import CalculatorContainer from './containers/CalculatorContainer';
-import './index.css';
+import reducer from './reducers'
+// import './index.css';
 
-ReactDOM.render(
-  <CalculatorContainer />,
+
+// ReactDOM.render(
+//   <CalculatorContainer />,
+//   document.getElementById('root')
+// );
+
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <CalculatorContainer />
+  </Provider>,
   document.getElementById('root')
-);
+)
